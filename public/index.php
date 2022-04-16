4bdb314d91f5beaab1887951d555893b3b52df5d
Guillaume BERTIN <80512116+Guibertinpro@users.noreply.github.com>
2022-04-16 11:14:37 +0200

    Insert user in database

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use App\Entity\User;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
@@ -19,3 +20,17 @@ $dbParams = [
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
 $entityManager = EntityManager::create($dbParams, $config);
+
+$user = new User();
+
+$user->setName("BERTIN")
+  ->setFirstname("Guillaume")
+  ->setUsername("Bob sinclar")
+  ->setPassword(password_hash('test', PASSWORD_BCRYPT))
+  ->setEmail("bob@gmail.com")
+  ->setBirthDate(new DateTime('1991-07-18'));
+
+  var_dump($user);
+
+  $entityManager->persist($user);
+  $entityManager->flush();
